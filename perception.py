3da6497cdfa3fa54342d06d6087e8dc0371369cd
zainbmaged <1901420@eng.asu.edu.eg>
2022-12-08 16:40:19 +0200

    Update perception.py

--- perception.py
+++ perception.py
@@ -1,22 +1,39 @@
 import numpy as np
 import cv2
 
-# Identify pixels above the threshold
-# Threshold of RGB > 160 does a nice job of identifying ground pixels only
+##------------------------------------------------------------------------------------------------------------------(1)
+    # Identify pixels above the threshold
+    # Threshold of RGB > 160 does a nice job of identifying ground pixels only
+## navigable train is tresholded with high values of RED GREEN BLUE (grey color as image will be processed in single channel)    
 def color_thresh(img, rgb_thresh=(160, 160, 160)):
-    # Create an array of zeros same xy size as img, but single channel
+    
+    # Create an array of zeros same xy size as img, but single channe  (z Represinting RGB is set to zero)
     color_select = np.zeros_like(img[:,:,0])
+    
     # Require that each pixel be above all three threshold values in RGB
     # above_thresh will now contain a boolean array with "True"
     # where threshold was met
+## each pixil with value higher than treshold in all three channels is set to one (white)
     above_thresh = (img[:,:,0] > rgb_thresh[0]) \
                 & (img[:,:,1] > rgb_thresh[1]) \
                 & (img[:,:,2] > rgb_thresh[2])
     # Index the array of zeros with the boolean array and set to 1
     color_select[above_thresh] = 1
-    # Return the binary image
+    # Return the binary image (only navigable train is set to one )
     return color_select
-
+## obstacles is tresholded with high values of RED & GREEN with less  BLUE (faint brown color is our treshold for obstacles) 
+def obstacles_tresh(img, obs_thresh=(120, 120, 90)):
+    color_select = np.zeros_like(img[:,:,0])
+    obs = (img[:,:,0] > obs_thresh[0]) & (img[:,:,1] > obs_thresh[1]) & (img[:,:,2] < obs_thresh[2])
+    color_select[obs] = 1
+    return color_select
+## obstacles is tresholded with high values of RED & GREEN with less  BLUE (faint brown color is our treshold for obstacles) 
+def rock_tresh(img, yellow_thresh=(120, 120, 10)):
+    color_select = np.zeros_like(img[:,:,0])
+    rock = (img[:,:,0] > yellow_thresh[0]) & (img[:,:,1] > yellow_thresh[1]) & (img[:,:,2] < yellow_thresh[2])
+    color_select[rock] = 1
+    return color_select
+##------------------------------------------------------------------------------------------------------------------(1)
 # Define a function to convert from image coords to rover coords
 def rover_coords(binary_img):
     # Identify nonzero pixels
