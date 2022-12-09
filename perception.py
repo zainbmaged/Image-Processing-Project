8985ae827b7e3bc2e564db15dffa211fbc1db8dc
zainbmaged <1901420@eng.asu.edu.eg>
2022-12-09 21:33:48 +0200

    Update perception.py

--- perception.py
+++ perception.py
@@ -27,7 +27,7 @@ def obstacles_tresh(img, obs_thresh=(120, 120, 90)):
     obstacle = (img[:,:,0] > obs_thresh[0]) & (img[:,:,1] > obs_thresh[1]) & (img[:,:,2] < obs_thresh[2])
     color_select[obstacle] = 1
     return color_select
-## obstacles is tresholded with high values of RED & GREEN with less  BLUE (faint brown color is our treshold for obstacles) 
+## obstacles is tresholded with high values of RED & GREEN with less  BLUE (shadow yellow color is our treshold for rocks 
 def rock_tresh(img, yellow_thresh=(120, 120, 10)):
     color_select = np.zeros_like(img[:,:,0])
     rock = (img[:,:,0] > yellow_thresh[0]) & (img[:,:,1] > yellow_thresh[1]) & (img[:,:,2] < yellow_thresh[2])
