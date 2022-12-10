5d275dc9fa2f06250bedd0bece954c1cccedf166
zainbmaged <1901420@eng.asu.edu.eg>
2022-12-10 20:31:22 +0200

    Update drive_rover.py

--- drive_rover.py
+++ drive_rover.py
@@ -61,7 +61,7 @@ class RoverState():
         # get creative in adding new fields or modifying these!
         self.stop_forward = 50 # Threshold to initiate stopping
         self.go_forward = 500 # Threshold to go forward again
-        self.max_vel = 2 # Maximum velocity (meters/second)
+        self.max_vel = 2.5 # Maximum velocity (meters/second)
         # Image output from perception step
         # Update this image to display your intermediate analysis steps
         # on screen in autonomous mode
